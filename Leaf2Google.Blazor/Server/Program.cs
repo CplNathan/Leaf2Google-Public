1182f003e9ebcba9a5ec2fc350376c5ba64b3b88
Nathan <16903800+CplNathan@users.noreply.github.com>
2023-01-04 14:11:44 +0000

    Removed https redirect as hosted behind reverse proxy and not needed.

--- Leaf2Google.Blazor/Server/Program.cs
+++ Leaf2Google.Blazor/Server/Program.cs
@@ -90,7 +90,6 @@ namespace Leaf2Google.Blazor
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseHttpsRedirection();
             }
             else
             {
